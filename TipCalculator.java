987cecf031e0f667d01a4704aa92689b3817aada
bgrove1974 <bgroveben@gmail.com>
2016-05-18 19:28:01 -0700

    Finish Tip Calculator

--- TipCalculator.java
+++ TipCalculator.java
@@ -3,7 +3,7 @@ public class TipCalculator {
     public static void main(String[] args) {
         double meal = 50.03;
         int tip = 20;
-        boolean splitCheck = false;
+        boolean splitCheck = true;
         int people = 1;
         String serverName = "Johannes";
 
@@ -13,8 +13,15 @@ public class TipCalculator {
         System.out.println("You are tipping " + tip + "% for a tip total of $" + tipAmount);
         System.out.println("Your meal total is: $" + mealTotal);
 
-        if (splitCheck == true) {
-            
+        if (splitCheck == true && people > 1) {
+            double each = mealTotal / people;
+            System.out.println("Each person owes: $" + each);
+        }
+        else if (people == 0) {
+            System.out.println("Are you sure there is nobody in your party?");
+        }
+        else {
+            System.out.println("Have a nice day.");
         }
     }
 }
